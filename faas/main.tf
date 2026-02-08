e6d65b56fa5bbb46a9c3257abaa9ed4f9204367d
guyzsarun <sarun.nunt@gmail.com>
2026-02-08 09:31:52 +0700

    fmt

--- faas/main.tf
+++ faas/main.tf
@@ -142,7 +142,7 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id           = aws_apigatewayv2_api.calculator_api.id
   integration_type = "AWS_PROXY"
 
-  description               = "Lambda example"
-  integration_method        = "POST"
-  integration_uri           = aws_lambda_function.faas.qualified_invoke_arn
+  description        = "Lambda example"
+  integration_method = "POST"
+  integration_uri    = aws_lambda_function.faas.qualified_invoke_arn
 }
